1e07de2b80866661f6711ce0324118c200fffd67
sailakshmi98 <132251010+sailakshmi98@users.noreply.github.com>
2023-10-24 14:46:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const billTotal = document.getElementById('billTotal');
+const tipSlider = document.getElementById('tipSlider');
+const tipPercentage = document.getElementById('tipPercentage');
+const tipAmount = document.getElementById('tipAmount');
+const totalBill = document.getElementById('totalBill');
+
+document.getElementById('tipForm').addEventListener('input', function() {
+    let bill = parseFloat(billTotal.value);
+    let tip = parseFloat(tipSlider.value);
+    
+    if (!isNaN(bill)) {
+        let tipValue = bill * (tip / 100);
+        let total = bill + tipValue;
+
+        tipPercentage.value = tip + '%';
+        tipAmount.value = tipValue.toFixed(2);
+        totalBill.value = total.toFixed(2);
+    } else {
+        alert('Please enter a valid bill total.');
+    }
+});
